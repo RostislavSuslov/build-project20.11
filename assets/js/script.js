575898d6039d0514766f16a7b1e41f0cf8a19f82
RostSuslov <suslovros@gmail.com>
2023-12-01 19:03:56 +0200

    feat: creat floor page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-const buildNav = document.querySelector('.builds-nav')
-const buildInfo = document.querySelector('.builds-info') 
+const installCalcHeight =()=> {
+    const buildNav = document.querySelector('.builds-nav')
+    const buildInfo = document.querySelector('.builds-info') 
 
-function reportWindowSize() {
-    const buildNavHeight = buildNav.offsetHeight
-    const buildInfoHeight = buildInfo.offsetHeight
-    const calcHeight = buildNavHeight - buildInfoHeight - 15;
-    buildInfo.style.top = calcHeight + "px" ;
-} reportWindowSize()
+    function reportWindowSize() {
+        const buildNavHeight = buildNav.offsetHeight
+        const buildInfoHeight = buildInfo.offsetHeight
+        const calcHeight = buildNavHeight - buildInfoHeight - 15;
+        buildInfo.style.top = calcHeight + "px" ;
+    } reportWindowSize()
 
-window.onresize = reportWindowSize;
+    window.onresize = reportWindowSize;
+}
 
-
+document.querySelector('.builds-list') ? installCalcHeight() : null;
 
 const installBuilds =()=> {
     const buildItem = document.querySelectorAll('.build-item path')
@@ -92,7 +94,201 @@ function installModal() {
 }
 document.querySelector('.custom-modal') ? installModal() : false;
 
- 
+
+const flatArray = [{
+    id: 0,
+    house: "2",
+    floor: "1",
+    rooms: "3",
+    square: "82.3 м²",
+    price: "700$",
+    priceTotal: "52500$",
+    flatNumber: 1,
+    status: "booking",
+},
+{
+    id: 1,
+    house: "2",
+    floor: "1",
+    rooms: "2",
+    square: "60,7 м²",
+    price: "700$",
+    priceTotal: "42000$",
+    flatNumber: 2,
+    status: "sold",
+},
+{
+    id: 2,
+    house: "2",
+    floor: "1",
+    rooms: "3",
+    square: "60,7 м²",
+    price: "700$",
+    priceTotal: "42000$",
+    flatNumber: 3,
+    status: "action",
+},
+{
+    id: 3,
+    house: "2",
+    floor: "1",
+    rooms: "3",
+    square: "82 м²",
+    price: "700$",
+    priceTotal: "52500$",
+    flatNumber: 4,
+    status: "booking",
+},
+{
+    id: 4,
+    house: "2",
+    floor: "1",
+    rooms: "3",
+    square: "79.7 м²",
+    price: "700$",
+    priceTotal: "55790$",
+    flatNumber: 5,
+    status: "booking",
+},
+{
+    id: 5,
+    house: "2",
+    floor: "1",
+    rooms: "1",
+    square: "39.2 м²",
+    price: "700$",
+    priceTotal: "27440$",
+    flatNumber: 6,
+    status: "free",
+},
+{
+    id: 6,
+    house: "2",
+    floor: "1",
+    rooms: "1",
+    square: "40.0 м²",
+    price: "700$",
+    priceTotal: "29400$",
+    flatNumber: 7,
+    status: "booking",
+},
+{
+    id: 7,
+    house: "2",
+    floor: "1",
+    rooms: "1",
+    square: "39.2 м²",
+    price: "700$",
+    priceTotal: "27440$",
+    flatNumber: 8,
+    status: "booking",
+},
+{
+    id: 8,
+    house: "2",
+    floor: "1",
+    rooms: "3",
+    square: "79.3 м²",
+    price: "700$",
+    priceTotal: "55510$",
+    flatNumber: 9,
+    status: "action",
+},
+]
+
+// const dog = {
+//     age: 3,
+//     hair: "red"
+// }
+
+// console.log(dog.age);
 
  
+window.addEventListener('load', function() {
+    function installFloorPlan() {
 
+        const flats = document.querySelectorAll('.flat');
+        const flatInfo = document.querySelector('.flat-info')
+        console.log(flatInfo);
+
+        const faltObj = [{
+            id: 4,
+            house: "2",
+            floor: "1",
+            rooms: "3",
+            square: "79.7 м²",
+            price: "700$",
+            priceTotal: "55790$",
+            flatNumber: 5,
+            status: "action",
+        }
+    ]
+
+        const renderInfo = (array) => {
+            console.log(array);
+            const flatInformation = array.map(item => {
+                return (`
+                <div class="flat-item">
+                    Номер дома : <b>${item.house}</b>
+                </div>
+                <div class="flat-item">
+                    Этаж : <b>${item.floor}</b>
+                </div>
+                <div class="flat-item">
+                    Кол-во комнат : <b>${item.rooms}</b>
+                </div>
+                <div class="flat-item">
+                    Площадь : <b>${item.square}</b>
+                </div>
+                <div class="flat-item">
+                    Цена за м² : <b>${item.price}</b>
+                </div>
+                <div class="flat-item">
+                    Цена за квартиру : <b>${item.priceTotal}</b>
+                </div>
+                <div class="flat-item">
+                    Номер квартиры : <b>${item.flatNumber}</b>
+                </div>
+                <div class="flat-item">
+                    Статус квартиры : <b>${item.status}</b>
+                </div>
+                
+                `)
+            })
+            flatInfo.innerHTML = flatInformation;
+        }
+        renderInfo(faltObj);
+
+        const removeActiveClass =()=> flats.forEach(item => {
+            item.classList.remove('active');
+        })
+
+        flats.forEach(flat => {
+            flat.addEventListener('mouseover', function(){
+                removeActiveClass();
+                flat.classList.add('active')
+
+                const thisFlat = flat.getAttribute('id')
+                console.log(thisFlat);
+
+                const flatNumber = flatArray.filter(item => item.flatNumber === Number(thisFlat))
+                console.table(flatNumber);
+
+                renderInfo(flatNumber);
+            })
+
+
+
+            if (flat.classList.contains('booking')) {
+                flat.querySelector('.flat-status').innerHTML = "Бронь";
+            } else if (flat.classList.contains('action')) {
+                flat.querySelector('.flat-status').innerHTML = "Акция";
+            } else if (flat.classList.contains('sold')){
+                flat.querySelector('.flat-status').innerHTML = "Продано";
+            } else {
+                flat.querySelector('.flat-status').innerHTML = "Свободна";
+            }
+        })
+    }
+    document.querySelector('.page-floor') ? installFloorPlan() : null;
+})
